c5c7e049eaa968de517be35a8c75decc0eb9df74
Enrique <easenrique@gmail.com>
2023-10-24 09:44:27 -0300

    Agrego signup

--- index.ts
+++ index.ts
@@ -9,11 +9,40 @@ const port = 3000;
 app.use(express.json());
 app.use(cors());
 
-app.post("/prueba", (req, res) => {
-  console.log(req.body);
-  res.json(req.body);
+const userCollection = firestore.collection("users");
+
+// signup
+
+app.post("/signup", (req, res) => {
+  const email = req.body.email;
+  const nombre = req.body.nombre;
+  userCollection
+    .where("email", "==", email)
+    .get()
+    .then((searchResponse) => {
+      if (searchResponse.empty) {
+        userCollection
+          .add({
+            email,
+            nombre,
+          })
+          .then((newUserRef) => {
+            res.json({
+              id: newUserRef.id,
+            });
+          });
+      } else {
+        console.log("El email ingresado ya se existe en la base de datos!");
+      }
+      res.json({});
+    });
+  // userCollection.doc("1234");
+  // console.log(req.body);
+  // res.json(req.body);
 });
 
+// authentication
+
 app.listen(port, () => {
-  console.log(`Inciciado en http://localhost:${port}`);
+  console.log(`Iniciado en http://localhost:${port}`);
 });
