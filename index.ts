5b4bd69c5c0f8cd0d5efae90f3b44a39d374cc20
EnriqueScianca <easenrique@gmail.com>
2023-10-24 12:29:23 -0300

    Agrego auth y algo mas

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { firestore, rtdb } from "./db";
 import * as express from "express";
-import { v4 as uuidv4 } from "uuid";
 import * as cors from "cors";
+import { nanoid } from "nanoid";
 
 const app = express();
 const port = 3000;
@@ -29,12 +29,15 @@ app.post("/signup", (req, res) => {
           .then((newUserRef) => {
             res.json({
               id: newUserRef.id,
+              new: true
             });
           });
       } else {
-        console.log("El email ingresado ya se existe en la base de datos!");
+        res.status(400).json({
+          message: "User already exists"
+        })
+        // console.log("El email ingresado ya se existe en la base de datos!");
       }
-      res.json({});
     });
   // userCollection.doc("1234");
   // console.log(req.body);
@@ -42,6 +45,59 @@ app.post("/signup", (req, res) => {
 });
 
 // authentication
+app.post("/auth", (req, res) => {
+  // La linea siguiente es lo mismo que la que le sigue, solo que para abreviarlo se utiliza el corchete en el nombre de la constante
+  // const email = req.body.email
+  const {email} = req.body 
+
+  userCollection
+    .where("email", "==", email)
+    .get()
+    .then((searchResponse) => {
+      if (searchResponse.empty) {
+        res.status(404).json({
+          message: "Not found"
+        })
+        // console.log("El email ingresado ya se existe en la base de datos!");
+        
+      } else {
+        res.json({
+          id:searchResponse.docs[0].id
+        })
+      }
+    });
+})
+
+app.post("/rooms", (req, res) => {
+  const {userId} = req.body
+  userCollection
+  .doc(userId.toString())
+  .get()
+  .then((doc) => {
+    if(doc.exists) {
+      rtdb
+      .ref("rooms/" + nanoid())
+      .set({
+        messages: [],
+        owner: userId
+      })
+      .then((rtdbRes) =>{
+         res.json({
+          id: rtdbRes.id,
+        })
+      })
+    }
+  })
+})
+
+
+
+app.get("rooms/:id", (req, res) => {
+  console.log(req.body)
+  res.json(req.body)
+})
+
+
 
 app.listen(port, () => {
   console.log(`Iniciado en http://localhost:${port}`);
